9a4c7ad23345596ba3cbb372f2e25abaa5c12fc8
HyuuunjuKim <gus7wn@gmail.com>
2019-07-08 17:03:11 +0900

    url상속 작업

--- underGround/urls.py
+++ underGround/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import Calender.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Calender.views.home, name="home"),
+    path('calender/', include('Calender.urls')),
+    path('carpool/', include('CarPool.urls')),
+    path('dating/', include('Dating.urls')),
+    path('findtheroom/', include('FindTHeRoom.urls')),
+    path('lostandfound/', include('LostAndFound.urls')),
 ]
