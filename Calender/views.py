b6b6f5c72ca8e5100c1b3a50351538bda923cff5
HyuuunjuKim <gus7wn@gmail.com>
2019-07-09 23:43:22 +0900

    새 글 등록까지 구현

--- Calender/views.py
+++ Calender/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Reservation
 
 # Create your views here.
 def real_home(request) :
     return render(request, 'real_home.html')
 
 def home(request) :
-    return render(request, 'calender/home.html')
+    reservations = Reservation.objects
+    return render(request, 'calender/home.html', {'reservations' : reservations})
+
+def detail(request, reservation_id) :
+    reservation_detail = get_object_or_404(Reservation, pk=reservation_id)
+    return render (request, 'calender/detail.html', {'reservation_detail' : reservation_detail})
+
+def new(request) :
+    return render(request, 'calender/new.html')
+
+def create(request) :
+    reservation = Reservation()
+    reservation.team = request.GET['team']
+    reservation.body = request.GET['body']
+    reservation.save()
+    return redirect('/calender/' + str(reservation.id))
