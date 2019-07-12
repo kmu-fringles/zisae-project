45195e8859822326ab3c2a51d2620eeeee0590e1
HyuuunjuKim <gus7wn@gmail.com>
2019-07-12 19:23:03 +0900

    분실물 CRUD 구현

--- LostAndFound/urls.py
+++ LostAndFound/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 import LostAndFound.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,6 +10,8 @@ urlpatterns = [
     path('<int:lost_id>/', LostAndFound.views.detail, name="detail" ),
     path('new/', LostAndFound.views.new, name="new"),
     path('create/', LostAndFound.views.create, name="create"),
-    
-]
+    path('<int:lost_id>/delete/', LostAndFound.views.delete, name="delete"), 
+    path('<int:lost_id>/edit/', LostAndFound.views.edit, name="edit"),
+    path('<int:lost_id>/update/', LostAndFound.views.update, name="update"),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- LostAndFound/views.py
+++ LostAndFound/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import LostAndFound
 from django.utils import timezone
 
+
 # Create your views here.
 def home(request) :
     losts = LostAndFound.objects
@@ -20,5 +21,24 @@ def create(request) :
     lost.writer = request.GET['writer']
     lost.pub_date = request.GET['pub_date']
     lost.body = request.GET['body']
+    lost.image= request.GET['image']
     lost.save()
     return redirect('/lostandfound/'+str(lost.id))
+
+def delete(request, lost_id) :
+    delete_lost = LostAndFound.objects.get(id=lost_id)
+    delete_lost.delete()
+    return redirect('lostandfound_home')
+
+def edit(request, lost_id) :
+    edit_lost = LostAndFound.objects.get(id = lost_id)
+    return render(request, 'lostandfound/edit.html', {'lost': edit_lost})
+
+def update(request, lost_id) :
+    update_lost = LostAndFound.objects.get(id = lost_id)
+    update_lost.title = request.POST['title']
+    update_lost.writer = request.POST['writer']
+    update_lost.pub_date = request.POST['pub_date']
+    update_lost.body = request.POST['body']
+    update_lost.save()
+    return redirect('lostandfound_home')
