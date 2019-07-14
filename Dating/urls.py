51c0fd8ab49661b730bea1b3e8bfa4a2b017032c
sehuikwon <sehui4011@hanmail.net>
2019-07-14 21:57:30 +0900

    upto CRUD

--- Dating/urls.py
+++ Dating/urls.py
@@ -4,5 +4,8 @@ import Dating.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', Dating.views.d_home, name='dating_home'),
+    path('', Dating.views.d_home, name='dating_home'),
+    path('new/', Dating.views.new, name='new'),
+    path('create/', Dating.views.create, name='create'),
+    path('<int:dating_id>/', Dating.views.detail, name='detail'),
 ]
--- Dating/views.py
+++ Dating/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
 from .models import Dating
 
@@ -8,3 +9,35 @@ def d_home(request):
     blogs= Dating.objects
     return render(request,'Dating/home.html',{'blogs':blogs})
 
+def new(request):
+    return render(request, 'Dating/new.html')
+
+
+def detail(request, dating_id):
+    dating_detail = get_object_or_404(Dating, pk=dating_id)
+    return render(request, 'Dating/detail.html', {'dating': dating_detail})
+
+def create(request):
+    dating = Dating()
+    dating.title = request.GET['title']
+    dating.body = request.GET['body']
+    dating.pub_date = timezone.datetime.now()
+    dating.save()
+    return redirect('/dating/'+str(dating.id))
+
+def delete(request, del_dating_id):
+    delete_post = get_object_or_404(Dating, pk=del_dating_id)
+    delete_post.delete()
+    return redirect('dating_home')
+
+def edit(request, edit_dating_id):
+    edit_post = get_object_or_404(Blog, pk=edit_dating_id)
+    return render(request, 'new.html', {'post':edit_post})
+
+def update(request, update_dating_id):
+    update_post = get_object_or_404(Dating, pk=update_dating_id)
+    update_post.title = request.POST['title']
+    update_post.writer = request.POST['writer']
+    update_post.body = request.POST['body']
+    update_post.save()
+    return redirect('dating_home')
